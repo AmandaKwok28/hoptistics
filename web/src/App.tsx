3b28a29183c25644f725aec5f967d62229470603
AmandaKwok <AmandaKwok28@gmail.com>
2024-12-22 16:42:31 -0800

    Set up the React project with Vite

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+
+function App() {
+  return (
+    <div className="flex justify-center items-center text-3xl min-h-screen">
+      <Button>Posts App</Button>
+    </div>
+  );
+}
+
+export default App;
